6f121cddd29386b93feba8888dea43d6028313ed
caoductam <ductam2024@gmail.com>
2025-03-27 21:47:14 +0700

    Initial commit

--- prs_project/settings.py
+++ prs_project/settings.py
@@ -69,10 +69,14 @@ WSGI_APPLICATION = 'prs_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moviegeek',                      
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
 # DATABASES = {
 #     'default': {
